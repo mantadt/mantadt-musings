9991329f60f444a0c0902a00019919876339162c
Aditya Manthena <aditya.manthena@pearson.com>
2015-09-29 16:46:18 +0530

    Rally API crawler changes

---
+++ src/Mantadt.RallyCrawler/Mantadt.RallyCrawler/RallyAPIConnector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pearson.RallyCrawler
+{
+    public static class RallyAPIConnector
+    {
+        public static RallyCrawlerDbContext _rcDB;
+
+        static RallyAPIConnector()
+        {
+            _rcDB = new RallyCrawlerDbContext();
+        }
+    }
+}
